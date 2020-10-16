df708e903ae6e89194d0929c9031d022f116357c
escm1990 <escm.1990@gmail.com>
2020-10-15 20:45:22 -0600

    2020-10-15 Bolsa de trabajo JSF - V8 (Select Items)

---
+++ BolsaTrabajoV8_JSF/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,104 @@
+package beans.model;
+
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+//anotaciones para que se convertido en managedBean
+@Named
+@RequestScoped
+public class Candidato {
+    private String nombre;
+    private String apellido;
+    private int salarioDeseado;
+    private Date fechaNacimiento;
+    private String codigoPostal;
+    private String ciudad;
+    private String comentario;
+    private int coloniaId;
+
+    public int getColoniaId() {
+        return coloniaId;
+    }
+
+    public void setColoniaId(int coloniaId) {
+        this.coloniaId = coloniaId;
+    }
+    
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Logger getLog() {
+        return log;
+    }
+
+    public void setLog(Logger log) {
+        this.log = log;
+    }
+
+    Logger log = LogManager.getRootLogger();
+    
+    public Candidato() {
+        log.info("Creando el objeto candidato");
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+        log.info("Modificando la propiedad de apellido "+this.apellido);
+    }
+
+    public int getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(int salarioDeseado) {
+        this.salarioDeseado = salarioDeseado;
+        log.info("Modificando la propiedad de salarioDeseado "+this.salarioDeseado);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        log.info("Modificando la propiedad de nombre "+this.nombre);
+    }
+    
+    
+}
